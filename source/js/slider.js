53356a40b4058e70be993c7c49a41069c0d09c68
maxkray54 <maxkray99@gmail.com>
2020-12-02 22:39:50 +0300

    Доверстать страницу формы

---
+++ source/js/slider.js
@@ -0,0 +1,20 @@
+// выбор нужного эелемента
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+var mainNavHead = document.querySelector('.main-nav__head');
+
+navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('main-nav--opened');
+navMain.classList.add('main-nav--closed');
+// ивент который срабатывает когда кликнули
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {// если закрыта
+    navMain.classList.remove('main-nav--closed');// удалить класс закрыто для выбранного выше эелеемнта
+    navMain.classList.add('main-nav--opened');// добавить класс открыто длы выбранного выше элемента
+    mainNavHead.classList.add('main-nav__head--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    mainNavHead.classList.remove('main-nav__head--opened');
+  }
+});
